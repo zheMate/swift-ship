f41b2f8dbb7e944e97be493ae69ca22d1165f035
Danil Vukolov <vukdan@permaviat.ru>
2023-04-02 22:34:19 +0500

    upgraded validation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-// регулярки для валидации
-const strRegex = /^[а-яА-Я\s]*$/; // проверка на стрингу
-const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-/* поддерживаемые форматы номеров - (123) 456-7890, (123)456-7890, 123-456-7890, 123.456.7890, 1234567890, +31636363634, 075-63546725 */
-const digitRegex = /^\d+$/; //проверка на цифры
-
-
 const cityRegionList = document.getElementById('city_region_list');
 const cityRegionSearch = document.getElementById('city_region_search');
 const fullScreenDiv = document.getElementById('fullscreen-div');
@@ -14,6 +6,19 @@ const closeBtn = document.getElementById('close-btn');
 const modalBtns = document.getElementById('modal-btns');
 const modalForm = document.getElementById('modal');
 const orderBookList = document.querySelector('#order-book-list tbody');
+const updateButton = document.getElementById('update-btn');
+const saveButton = document.getElementById('save-btn');
+
+// const addressingNameForValidation = document.getElementById('addr_ing_name');
+// const firstNameForValidation = document.getElementById('first_name');
+// const lastNameForValidation = document.getElementById('last_name');
+// const emailForValidation = document.getElementById('email');
+// const phoneForValidation = document.getElementById('phone');
+// const streetAddrForValidation = document.getElementById('street_addr');
+// const postalCodeForValidation = document.getElementById('postal_code');
+// const cityRegionForValidation = document.getElementById('city_region');
+// const labelsForValidation = document.getElementById('labels');
+
 
 // значение по умолчанию
 let addrName = firstName = lastName = email = phone = streetAddr = postCode = cityAndRegion = labels = "";
@@ -196,13 +201,10 @@ function eventListeners() {
         if (event.target.id == "save-btn") {
             let isFormValid = getFormData();
             if (!isFormValid) {
-                modalForm.querySelectorAll('input').forEach(input => {
-                    setTimeout(() => {
-                        input.classList.remove('errorMsg');
-                    }, 1500);
-                });
+                saveButton.disabled = true;
             }
             else {
+                saveButton.disabled = false;
                 let allItem = Order.getOrders();
                 let lastItemId = (allItem.length > 0) ? allItem[allItem.length - 1].id : 0;
                 lastItemId++;
@@ -247,13 +249,10 @@ function eventListeners() {
             let id = event.target.dataset.id;
             let isFormValid = getFormData();
             if (!isFormValid) {
-                modalForm.querySelectorAll('input').forEach(input => {
-                    setTimeout(() => {
-                        input.classList.remove('errorMsg');
-                    }, 1500);
-                });
+                updateButton.disabled = true;
             }
             else {
+                updateButton.disabled = false;
                 const orderItem = new Order(id, addrName, firstName, lastName, email, phone, streetAddr, postCode, cityAndRegion, labels);
                 Order.updateOrder(orderItem);
                 UI.closeModal();
@@ -290,73 +289,114 @@ function getFormData() {
           modalForm.city_region.value, 
           modalForm.labels.value); */
 
-    if (!strRegex.test(modalForm.addr_ing_name.value) || modalForm.addr_ing_name.value.trim().length == 0) {
-        addErrMsg(modalForm.addr_ing_name);
+    const addressingNameForValidation = modalForm.addr_ing_name.value.trim();
+    const firstNameForValidation = modalForm.first_name.value.trim();
+    const lastNameForValidation = modalForm.last_name.value.trim();
+    const emailForValidation = modalForm.email.value.trim();
+    const phoneForValidation = modalForm.phone.value.trim();
+    const streetAddressForValidation = modalForm.street_addr.value.trim();
+    const postalCodeForValidation = modalForm.postal_code.value.trim();
+    
+
+    const setError = (element, message) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success');
+    }
+    const setSuccess = element => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error');
+    }
+    const isValidEmail = email => {
+        const re = /^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
+        return re.test(String(email).toLowerCase());
+    }
+    const isValidPhone = phone => {
+        const re = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+        return re.test(phone);
+    }
+
+    if (addressingNameForValidation === '') {
         inputValidStatus[0] = false;
-    } else {
-        addrName = modalForm.addr_ing_name.value;
+        setError(modalForm.addr_ing_name, 'Имя получателя, обязательное поле');
+    }
+    else {
         inputValidStatus[0] = true;
+        setSuccess(modalForm.addr_ing_name);
+        addrName = modalForm.addr_ing_name.value;
     }
-
-    if (!strRegex.test(modalForm.first_name.value) || modalForm.first_name.value.trim().length == 0) {
-        addErrMsg(modalForm.first_name);
+    if (firstNameForValidation === '') {
         inputValidStatus[1] = false;
-    } else {
-        firstName = modalForm.first_name.value;
+        setError(modalForm.first_name, 'Имя, обязательное поле');
+    }
+    else {
         inputValidStatus[1] = true;
+        setSuccess(modalForm.first_name);
+        firstName = modalForm.first_name.value;
     }
-
-    if (!strRegex.test(modalForm.last_name.value) || modalForm.last_name.value.trim().length == 0) {
-        addErrMsg(modalForm.last_name);
+    if (lastNameForValidation === '') {
         inputValidStatus[2] = false;
-    } else {
-        lastName = modalForm.last_name.value;
+        setError(modalForm.last_name, 'Фамилия, обязательное поле');
+    }
+    else {
         inputValidStatus[2] = true;
+        lastName = modalForm.last_name.value;
+        setSuccess(modalForm.last_name);
     }
-
-    if (!emailRegex.test(modalForm.email.value)) {
-        addErrMsg(modalForm.email);
+    if (emailForValidation === '') {
         inputValidStatus[3] = false;
-    } else {
-        email = modalForm.email.value;
+        setError(modalForm.email, 'Email, обязательное поле');
+    }
+    else if (!isValidEmail(emailForValidation)) {
+        inputValidStatus[3] = false;
+        setError(modalForm.email, 'Введите корректный Email');
+    }
+    else {
         inputValidStatus[3] = true;
+        email = modalForm.email.value;
+        setSuccess(modalForm.email);
     }
 
-    if (!phoneRegex.test(modalForm.phone.value)) {
-        addErrMsg(modalForm.phone);
+    if (phoneForValidation === '') {
         inputValidStatus[4] = false;
-    } else {
-        phone = modalForm.phone.value;
+        setError(modalForm.phone, 'Телефон, обязательное поле');
+    }
+    else if (!isValidPhone(phoneForValidation)) {
+        inputValidStatus[4] = false;
+        setError(modalForm.phone, 'Введите корректный номер');
+    }
+    else {
         inputValidStatus[4] = true;
+        phone = modalForm.phone.value;
+        setSuccess(modalForm.phone);
     }
-
-    if (!(modalForm.street_addr.value.trim().length > 0)) {
-        addErrMsg(modalForm.street_addr);
+    if (streetAddressForValidation === '') {
         inputValidStatus[5] = false;
-    } else {
-        streetAddr = modalForm.street_addr.value;
+        setError(modalForm.street_addr, 'Адрес, обязательное поле');
+    }
+    else {
         inputValidStatus[5] = true;
+        streetAddr = modalForm.street_addr.value;
+        setSuccess(modalForm.street_addr);
     }
-
-    if (!digitRegex.test(modalForm.postal_code.value)) {
-        addErrMsg(modalForm.postal_code);
+    if (postalCodeForValidation === '') {
         inputValidStatus[6] = false;
-    } else {
-        postCode = modalForm.postal_code.value;
+        setError(modalForm.postal_code, 'Почтовый индекс, обязательное поле');
+    }
+    else {
         inputValidStatus[6] = true;
+        postCode = modalForm.postal_code.value;
+        setSuccess(modalForm.postal_code);
     }
-
-    if (!strRegex.test(modalForm.city_region.value) || modalForm.city_region.value.trim().length == 0) {
-        addErrMsg(modalForm.city_region);
-        inputValidStatus[7] = false;
-    } else {
-        cityAndRegion = modalForm.city_region.value;
-        inputValidStatus[7] = true;
-    }
+    cityAndRegion = modalForm.city_region.value;
     labels = modalForm.labels.value;
     return inputValidStatus.includes(false) ? false : true;
 }
 
-function addErrMsg(inputBox) {
-    inputBox.classList.add('errorMsg');
-}
